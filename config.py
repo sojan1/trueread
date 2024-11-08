8a94af63baa528b3ef7d1d25fc825ad8ebe90761
sojan1 <sojanctherakom@gmail.com>
2024-11-09 00:48:38 +1300

    changed layout menus

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 class Config:
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "dbpassword")
-    #DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_HOST = os.getenv("DB_HOST", "db")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    #DB_HOST = os.getenv("DB_HOST", "db")
     DB_PORT = os.getenv("DB_PORT", "5432")
     DB_NAME = os.getenv("DB_NAME", "isaid")
 
