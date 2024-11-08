5ffb607e77eb27f4e902278d7483b7183f8f5fd8
sojan1 <sojanctherakom@gmail.com>
2024-11-08 17:32:00 +1300

    psycopg2-binary error , uninstalled and installed again and it worked.. what creates a database is not what runs

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "dbpassword")
     DB_HOST = os.getenv("DB_HOST", "localhost")
+    #DB_HOST = os.getenv("DB_HOST", "db")
     DB_PORT = os.getenv("DB_PORT", "5432")
     DB_NAME = os.getenv("DB_NAME", "isaid")
 
